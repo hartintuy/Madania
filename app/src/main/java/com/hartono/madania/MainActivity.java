5a022a3200311453503cd838ac23be80e11f7121
rachmad.hartono <rachmad.hartono@uii.ac.id>
2022-03-23 10:10:23 +0700

    Initial commit

---
+++ app/src/main/java/com/hartono/madania/MainActivity.java
@@ -0,0 +1,12 @@
+package com.hartono.madania;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+public class MainActivity exten {
+
+
+    }
+}
